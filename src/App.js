0fa098f9f9c03f00e4272fa53fea2af2f80c6562
Vanessa Martin <vanessa.s.martin96@gmail.com>
2020-12-28 23:30:37 -0800

    changing package manager to yarn only

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">hullo?</div>;
 }
 
 export default App;

c0daff1ff425c44e422c6f81e4c46e2960075665
Vanessa Martin <vanessa.s.martin96@gmail.com>
2021-01-05 14:28:10 -0800

    changing certain spans to divs

--- src/components/common/product-card/ProductCard.jsx
+++ src/components/common/product-card/ProductCard.jsx
@@ -6,20 +6,20 @@ import './ProductCard.css'
 const ProductCard = ({ index, card }) => {
   return (
     <div className={index === 1 ? 'card card2-bg' : 'card card1-bg'}>
-      <span className="card__tier">
+      <div className="card__tier">
         <h2>{card.tier}</h2>
-      </span>
+      </div>
 
-      <span className="card__feature">
+      <div className="card__feature">
         <p className="card__price">{card.price !== 0 ? `$${card.price}` : 'Free'}</p>
         <p>{card.tierFeature}</p>
-      </span>
+      </div>
 
-      <span className={index === 1 ? 'card__bonus card__bonus-white' : 'card__bonus card__bonus-grey'}>
+      <div className={index === 1 ? 'card__bonus card__bonus-white' : 'card__bonus card__bonus-grey'}>
         <p>{card.bonus1}</p>
         <p>{card.storage}</p>
         <p>{card.bonus2}</p>
-      </span>
+      </div>
 
       <Link to="/sign-up" className={index === 1 ? 'card-btn card-btn2' : 'card-btn card-btn1'}>Try for Free</Link>
     </div>);
